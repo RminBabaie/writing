6018d195156ebe8555f68d141cf5be4d63625b4f
Armin babaie <ahabtheone@gmail.com>
2021-01-30 20:20:14 +0330

    json files

--- username.py
+++ username.py
@@ -4,7 +4,3 @@ username=input("please enter your user name==>")
 with open(filename,"w") as f:
     json.dump(username,f)
 print("thank you , we will remeber this next time")
-filename1="username.json"
-with open(filename) as J:
-    username1=json.load(filename1)
-    print(f"welcome back {username1}")
